3538e04b8cf4a3b004ce5313dc8fb21a3f78f056
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-27 00:29:08 +0900

    pattern1

--- 241226_q02_condition/src/App.vue
+++ 241226_q02_condition/src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { ref } from 'vue';
+const isOpen = ref(false);
 
+function toggle() {
+  isOpen.value = !isOpen.value;
+}
 </script>
 
 <template>
+  <button @click="toggle">Toggle Message</button>
 
+  <p v-if="isOpen">Hello Vue!</p>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
