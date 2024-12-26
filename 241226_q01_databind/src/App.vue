a19116697e4f5c269bc44cc4439b594dea8ed382
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-27 00:11:34 +0900

    q01 clear

--- 241226_q01_databind/src/App.vue
+++ 241226_q01_databind/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-
+const greeting = ['test', 'test2'];
 </script>
 
 <template>
-
+  <h1>{{ greeting }}</h1>
+  {{ greeting[0] }}
+  {{ greeting[1] }}
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
