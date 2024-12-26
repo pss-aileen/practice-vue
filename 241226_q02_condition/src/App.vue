35b7bc95fd844f6ff5d072a1f275a0c6bceffbde
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-27 00:54:16 +0900

    241226 q02 condition ok (#1)
    * init
    
    * いらないコード削除
    
    * pattern1
    
    * pattern2
    
    * correct code
    
    * v-show example
    
    * simple transition example

---
+++ 241226_q02_condition/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+const isOpen = ref(false);
+
+function toggle() {
+  isOpen.value = !isOpen.value;
+}
+</script>
+
+<template>
+  <button @click="toggle">Toggle Message</button>
+  <Transition>
+    <p v-show="isOpen">Hello Vue!</p>
+  </Transition>
+  <p>他のテキスト</p>
+  <!-- <p v-show="isOpen">Goodbye Vue!</p> -->
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
