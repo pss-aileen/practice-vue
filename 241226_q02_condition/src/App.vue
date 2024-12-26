5dd79dba44377d03220e8663ae12d8d178ce282c
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-27 00:35:17 +0900

    correct code

--- 241226_q02_condition/src/App.vue
+++ 241226_q02_condition/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 const isOpen = ref(false);
+
+function toggle() {
+  isOpen.value = !isOpen.value;
+}
 </script>
 
 <template>
-  <!-- <button @click="toggle">Toggle Message</button> -->
-  <button @click="isOpen = !isOpen">Toggle Message</button>
+  <button @click="toggle">Toggle Message</button>
 
   <p v-if="isOpen">Hello Vue!</p>
 </template>
