a6fdbbfbc483808e6cadee20758241953d25bcf9
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-27 22:35:32 +0900

    241227 q5 loop (#4)
    * init
    
    * loop

---
+++ 241227_q5_loop/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+const movies = ['Inception', 'The Matrix', 'Interstellar', 'Spirited Away', 'The Godfather'];
+</script>
+
+<template>
+  <h1>Favorite Movies</h1>
+  <ul>
+    <li v-for="(movie, index) in movies" :key="index">
+      {{ movie }}
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
