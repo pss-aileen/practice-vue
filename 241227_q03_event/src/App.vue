656ab09ce803f7b7803d7f9b43de01a902c458ef
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-27 10:50:42 +0900

    241227 q3 event (#2)
    * init
    
    * いらないコード削除
    
    * pattern1
    
    * pattern2
    
    * correct code
    
    * v-show example
    
    * simple transition example
    
    * init
    
    * increment, decrement, reset
    
    * done

---
+++ 241227_q03_event/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+function decrement() {
+  // 0以下にならないようにする
+  if (count.value <= 0) {
+    return;
+  }
+  count.value--;
+}
+function reset() {
+  count.value = 0;
+}
+</script>
+
+<template>
+  <p>現在のカウント数 {{ count }}</p>
+  <p v-show="count >= 10">やったね！今10以上です！</p>
+  <ul>
+    <li>
+      <button @click="increment">increment counter</button>
+    </li>
+    <li>
+      <button @click="decrement">decrement counter</button>
+    </li>
+    <li>
+      <button @click="reset">reset counter</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
