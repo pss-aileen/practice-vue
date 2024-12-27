3872855f0e9c939b8bfd53a6672f6c0fa3f7b706
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-27 22:31:47 +0900

    loop

--- 241227_q5_loop/src/App.vue
+++ 241227_q5_loop/src/App.vue
@@ -1,30 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+const movies = ['Inception', 'The Matrix', 'Interstellar', 'Spirited Away', 'The Godfather'];
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Favorite Movies</h1>
+  <ul>
+    <li v-for="(movie, index) in movies" :key="index">
+      {{ movie }}
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
