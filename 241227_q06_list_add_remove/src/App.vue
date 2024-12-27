3468f4dc964b42250f1ce68aa95e702c2344e077
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-27 22:53:40 +0900

    q06 my answer

---
+++ 241227_q06_list_add_remove/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+const tasks = ref([]);
+const input = ref(null);
+
+function addTask(e) {
+  e.preventDefault();
+  tasks.value.push(input.value);
+  input.value = null;
+}
+
+function removeTask(e) {
+  const parentNode = e.target.parentNode;
+  parentNode.remove();
+}
+</script>
+
+<template>
+  <h1>リスト</h1>
+
+  <form>
+    <label>
+      tasks
+      <input type="text" v-model="input" />
+    </label>
+    <button @click="addTask">add</button>
+  </form>
+
+  <ul>
+    <li v-for="(task, index) in tasks" :key="index">
+      {{ task }}
+
+      <button @click="removeTask">x</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
