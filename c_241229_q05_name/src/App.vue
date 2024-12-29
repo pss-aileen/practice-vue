cdd0ed5a5e8af1d591f8b7e296411baa242489ec
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-29 17:08:50 +0900

    c_241229_q05_name 基本的な名前フィルター

---
+++ c_241229_q05_name/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const input = ref('');
+
+const userList = ref(['太郎', '花子', '次郎', '三郎', 'ねこ', 'いぬ', 'あられ', '子太郎']);
+
+const filteredUserList = computed(() => {
+  if (input.value === '') {
+    return userList.value;
+  } else {
+    return userList.value.filter((user) => {
+      return user.includes(input.value);
+    });
+  }
+});
+</script>
+
+<template>
+  <input type="text" v-model="input" />
+  <ul>
+    <li v-for="(user, index) in filteredUserList" :key="index">
+      {{ user }}
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
