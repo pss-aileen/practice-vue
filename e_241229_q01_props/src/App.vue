f53b656cbc8729b68607ef33c361eff15cc83627
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-29 20:08:24 +0900

    emit 01 my code

---
+++ e_241229_q01_props/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Child from './components/Child.vue';
+
+const text: string = 'Hello Vue!';
+</script>
+
+<template>
+  <p>Parent: {{ text }}</p>
+
+  <Child :text="text" />
+</template>
+
+<style scoped></style>
---
+++ e_241229_q01_props/src/components/Child.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: String,
+});
+
+function Notice() {
+  console.log(props.text);
+}
+</script>
+<template>
+  <p>Child: {{ text }}</p>
+  <button @click="Notice">Notice</button>
+</template>
+
+<style scoped></style>
