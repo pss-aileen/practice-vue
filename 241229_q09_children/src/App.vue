77c8f71346ca267fdcd6044f19ddc9d82237c57a
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-29 18:22:21 +0900

    ts

--- 241229_q09_children/src/App.vue
+++ 241229_q09_children/src/App.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import List from './components/List.vue';
 
-<template></template>
+const lists: string[] = ['いち', 'にー', 'さん', 'しー', 'ご'];
+</script>
+
+<template>
+  <ul>
+    <li v-for="(item, index) in lists" :key="index">
+      <List :name="item" />
+    </li>
+  </ul>
+</template>
 
 <style scoped></style>
---
+++ 241229_q09_children/src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ props.name }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
