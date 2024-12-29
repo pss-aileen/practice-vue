58e53d214c7d9434dc26fe4e1f5c57cfafb9ba7c
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-29 17:29:28 +0900

    241229_q08_watch

---
+++ 241229_q08_watch/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const inputText = ref('');
+
+watch(inputText, (newVal, oldVal) => {
+  console.log(`値が変わりました: ${oldVal} → ${newVal}`);
+});
+
+const textLength = ref(0);
+watch(inputText, (newVal) => {
+  textLength.value = newVal.length;
+});
+</script>
+
+<template>
+  <p>{{ textLength }}</p>
+  <input type="text" v-model="inputText" />
+</template>
+
+<style scoped></style>
