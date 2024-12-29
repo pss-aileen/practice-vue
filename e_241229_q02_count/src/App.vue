f7652f373b5669d5dad7985240b7fb8a5c626bea
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-29 20:20:46 +0900

    e q02 ???

---
+++ e_241229_q02_count/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ChildButton from './components/ChildButton.vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>Count</h1>
+  <p>{{ count }}</p>
+  <ChildButton @addCount="(c) => count + c" />
+</template>
+
+<style scoped></style>
---
+++ e_241229_q02_count/src/components/ChildButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// const emit = defineEmits(['addCount']);
+
+const emit = defineEmits<{
+  addCount: [count: number];
+}>;
+
+function handleClick() {
+  emit('addCount', 1);
+}
+</script>
+
+<template>
+  <button>add count</button>
+</template>
+
+<style scoped></style>
