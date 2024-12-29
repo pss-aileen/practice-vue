1eaeb547c78987eae74a08493afcfa04b3ca339b
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-29 20:00:30 +0900

    q10 emit pending

---
+++ 241229_q10_emit/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
+
+const childMessage = ref('I do not recieve message.');
+</script>
+
+<template>
+  <ChildComponent @response="(msg) => (childMessage = msg)" />
+  <p>{{ childMessage }}</p>
+</template>
+
+<style scoped></style>
---
+++ 241229_q10_emit/src/components/ChildComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const emit = defineEmits(['response']);
+emit('response', 'CHILD MESSAGE!');
+</script>
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
