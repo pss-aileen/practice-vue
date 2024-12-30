30f55259c7eb09727a21eb6a26aa335978422693
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-30 23:27:15 +0900

    my answer

--- e_241229_q02_count/src/App.vue
+++ e_241229_q02_count/src/App.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import ChildButton from './components/ChildButton.vue';
+import ChildComponent from './components/ChildComponent.vue';
 
 const count = ref(0);
+
+// 4.結果的にここでカウントが追加される
+function addCount() {
+  count.value++;
+}
 </script>
 
 <template>
-  <h1>Count</h1>
-  <p>{{ count }}</p>
-  <ChildButton @addCount="(c) => count + c" />
+  <h2>Count: {{ count }}</h2>
+
+  <p>
+    <button @click="addCount">親コンポーネント追加</button>
+  </p>
+  <!-- 3.子コンポーネントのincrementがクリック？放出されたら、addCountを発火する -->
+  <ChildComponent @increment="addCount" />
 </template>
 
 <style scoped></style>
---
+++ e_241229_q02_count/src/components/ChildComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// 1.命令、イベントを定義する
+const emit = defineEmits(['increment']);
+
+// 2.ボタンをクリックしたら、イベント定義したものを親コンポーネントに渡す
+function handleClick() {
+  emit('increment');
+  console.log('emit');
+}
+</script>
+<template>
+  <p>
+    <button @click="handleClick">子コンポーネント追加</button>
+  </p>
+</template>
+
+<style scoped></style>
