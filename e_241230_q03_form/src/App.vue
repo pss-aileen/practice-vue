709f9f22d01045b059bd6a7a9dfaba449ba93e0f
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-30 23:43:54 +0900

    e q03 count my answer

---
+++ e_241230_q03_form/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ChildComp from './components/ChildComp.vue';
+
+const message = ref('');
+
+function deleteMessage() {
+  message.value = '';
+}
+</script>
+
+<template>
+  <input type="text" v-model="message" />
+  <p>parentMessage: {{ message }}</p>
+  <ChildComp :message="message" @delete-message="deleteMessage" />
+</template>
+
+<style scoped></style>
---
+++ e_241230_q03_form/src/components/ChildComp.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  message: String,
+});
+
+const emit = defineEmits(['deleteMessage']);
+
+function handleClick() {
+  emit('deleteMessage');
+  console.log('emit delete Message');
+}
+</script>
+<template>
+  <p>childComponent: {{ props.message }}</p>
+  <button @click="handleClick">delete message</button>
+</template>
+
+<style scoped></style>
