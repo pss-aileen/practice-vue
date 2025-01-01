1a7bf6e45691db949ff5c2151153c6603efed765
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-02 01:22:25 +0900

    emit 05 done

--- emit/250102_q05_data/src/App.vue
+++ emit/250102_q05_data/src/App.vue
@@ -1,5 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+import InputForm from './components/InputForm.vue';
 
-<template></template>
+interface itemsProps {
+  id: number;
+  title: string;
+  createdAt: number;
+}
+
+const items: Ref<itemsProps[]> = ref([]);
+
+let count = 0;
+function addItem(title: string) {
+  const item: itemsProps = {
+    id: count++,
+    title: title,
+    createdAt: new Date().getTime(),
+  };
+
+  items.value.push(item);
+
+  console.log(items.value);
+}
+</script>
+
+<template>
+  <h1>タスク一覧</h1>
+  <InputForm @add-task="addItem" />
+  <ul>
+    <li v-for="item in items" :key="item.id">{{ item.id }}: {{ item.title }} - {{ item.createdAt }}</li>
+  </ul>
+</template>
 
 <style scoped></style>
---
+++ emit/250102_q05_data/src/components/InputForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+
+const emit = defineEmits<{
+  (e: 'add-task', title: string): void;
+}>();
+
+const title: Ref<string> = ref('');
+
+function handleClick(e: Event) {
+  e.preventDefault();
+  emit('add-task', title.value);
+  title.value = '';
+}
+</script>
+<template>
+  <form>
+    <input type="text" v-model="title" />
+
+    <button @click="handleClick">add item</button>
+  </form>
+</template>
+
+<style scoped></style>
