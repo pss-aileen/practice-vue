fce43c0d1031b880fa1818887d02ee70dafe3d7a
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-03 15:50:21 +0900

    delete task

---
+++ todolist/250103_03_completed/src/App.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+
+let idCount: number = 0
+
+const taskInput: Ref<string> = ref('')
+
+type taskType = {
+  id: number
+  title: string
+  isCompleted: boolean
+}
+
+const tasks: Ref<taskType[]> = ref([])
+
+function addTask(e: Event) {
+  e.preventDefault()
+  const task: taskType = {
+    id: idCount++,
+    title: taskInput.value,
+    isCompleted: false,
+  }
+  tasks.value.push(task)
+  taskInput.value = ''
+  console.log(tasks.value)
+}
+
+function deleteTask(id: number) {
+  const newTasks = tasks.value.filter((task) => task.id !== id)
+  tasks.value = newTasks
+}
+
+function changeCompleted(boolean: boolean) {
+  console.log(boolean)
+}
+</script>
+
+<template>
+  <section class="wrapper">
+    <h1>TASKS LIST</h1>
+
+    <form>
+      <input type="text" v-model="taskInput" />
+      <button @click="addTask">add</button>
+    </form>
+
+    <ul v-if="tasks.length !== 0">
+      <li v-for="task in tasks" :key="task.id">
+        <input
+          type="checkbox"
+          v-model="task.isCompleted"
+          @change="changeCompleted(task.isCompleted)"
+        />
+        {{ task.title }}
+        <button @click="deleteTask(task.id)">🗑️</button>
+      </li>
+    </ul>
+    <p v-else>There is no task.</p>
+  </section>
+</template>
+
+<style scoped>
+.wrapper {
+  max-width: 400px;
+  margin: 0 auto;
+
+  form {
+    display: flex;
+    gap: 4px;
+
+    input {
+      flex: 1;
+      padding: 4px;
+    }
+
+    button {
+      width: 80px;
+      padding: 4px;
+    }
+  }
+}
+</style>
