02d07c4594f21b76ee91562431331d5f70d746aa
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-03 15:33:52 +0900

    add task

--- todolist/250103_todolist/src/App.vue
+++ todolist/250103_todolist/src/App.vue
@@ -1,5 +1,64 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
 
-<template></template>
+let idCount: number = 0
 
-<style scoped></style>
+const taskInput: Ref<string> = ref('')
+
+type taskType = {
+  id: number
+  title: string
+}
+
+const tasks: Ref<taskType[]> = ref([])
+
+function addTask(e: Event) {
+  e.preventDefault()
+  const task: taskType = {
+    id: idCount++,
+    title: taskInput.value,
+  }
+  tasks.value.push(task)
+  taskInput.value = ''
+}
+</script>
+
+<template>
+  <section class="wrapper">
+    <h1>TASKS LIST</h1>
+
+    <form>
+      <input type="text" v-model="taskInput" />
+      <button @click="addTask">add</button>
+    </form>
+
+    <ul v-if="tasks.length !== 0">
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }}
+      </li>
+    </ul>
+    <p v-else>There is no task.</p>
+  </section>
+</template>
+
+<style scoped>
+.wrapper {
+  max-width: 400px;
+  margin: 0 auto;
+
+  form {
+    display: flex;
+    gap: 4px;
+
+    input {
+      flex: 1;
+      padding: 4px;
+    }
+
+    button {
+      width: 80px;
+      padding: 4px;
+    }
+  }
+}
+</style>
