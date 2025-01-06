113c5924200131e6cb17302ce555e1a4f8e5e094
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-06 23:26:48 +0900

    basic route

--- vueRouter/2250106_getting-started/src/App.vue
+++ vueRouter/2250106_getting-started/src/App.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
 
-<template></template>
+<template>
+  <h1>App.vue</h1>
+  <nav>
+    <RouterLink to="/">HOME</RouterLink>
+    <RouterLink to="/about">ABOUT</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+</template>
 
 <style scoped></style>
--- vueRouter/2250106_getting-started/src/main.ts
+++ vueRouter/2250106_getting-started/src/main.ts
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import HomeView from './HomeView.vue';
+import AboutView from './AboutView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    component: AboutView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount('#app');
