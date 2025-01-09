1f0114326187ba75c0af9a462c60d30dd6264354
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-09 11:17:16 +0900

    delete function

--- todolist2/250109/src/App.vue
+++ todolist2/250109/src/App.vue
@@ -1,33 +1,44 @@
 <script setup lang="ts">
 import { ref, type Ref } from 'vue';
 
-/* 
+/*
   基本のTODO作成
-  - 追加
-  - 表示
+  - 追加: done
+  - 表示: done
   - 削除
   - 編集
   - ローカルストレージ保存
 */
 
+// todoのtitleの準備
 const inputTitle = ref('');
 
+// Todoの型
 type TodoType = {
+  id: number;
   title: string;
 };
 
+// Todosの準備
 const todos: Ref<TodoType[]> = ref([]);
 
+// 追加機能
 function addTodo() {
   const todo: TodoType = {
+    id: new Date().getTime(),
     title: inputTitle.value,
   };
 
   todos.value.push(todo);
+  inputTitle.value = '';
 
   console.table(todos.value);
+}
 
-  inputTitle.value = '';
+function deleteTodo(id: number) {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+
+  console.table(todos.value);
 }
 </script>
 
@@ -41,7 +52,10 @@ function addTodo() {
     </form>
 
     <ul v-if="todos.length !== 0">
-      <li v-for="(todo, index) in todos" :key="index">{{ todo.title }}</li>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.title }}
+        <button @click="deleteTodo(todo.id)">❌</button>
+      </li>
     </ul>
   </section>
 </template>
