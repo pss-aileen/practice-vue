4de0b36545fd85a4e76980364f9579012cae904f
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-09 11:12:27 +0900

    function add

--- todolist2/250109/src/App.vue
+++ todolist2/250109/src/App.vue
@@ -1,5 +1,63 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
 
-<template></template>
+/* 
+  基本のTODO作成
+  - 追加
+  - 表示
+  - 削除
+  - 編集
+  - ローカルストレージ保存
+*/
 
-<style scoped></style>
+const inputTitle = ref('');
+
+type TodoType = {
+  title: string;
+};
+
+const todos: Ref<TodoType[]> = ref([]);
+
+function addTodo() {
+  const todo: TodoType = {
+    title: inputTitle.value,
+  };
+
+  todos.value.push(todo);
+
+  console.table(todos.value);
+
+  inputTitle.value = '';
+}
+</script>
+
+<template>
+  <section>
+    <h1>TODO LIST</h1>
+
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="inputTitle" />
+      <button>add</button>
+    </form>
+
+    <ul v-if="todos.length !== 0">
+      <li v-for="(todo, index) in todos" :key="index">{{ todo.title }}</li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+section {
+  max-width: 480px;
+  margin: 0 auto;
+
+  form {
+    input {
+      width: 100%;
+    }
+
+    button {
+    }
+  }
+}
+</style>
