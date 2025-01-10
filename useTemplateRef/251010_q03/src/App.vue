015e65f348930a0ff1faf94c5dbedb050a94306c
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-10 11:20:24 +0900

    templateRefの見本

--- useTemplateRef/251010_q03/src/App.vue
+++ useTemplateRef/251010_q03/src/App.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref, useTemplateRef } from 'vue';
 
-<template></template>
+const list = ref([1, 2, 3]);
 
-<style scoped></style>
+const itemRefs = useTemplateRef('items');
+
+onMounted(() => {
+  console.log(itemRefs.value);
+  console.log(itemRefs.value?.[1]);
+  itemRefs.value?.[0].classList.add('test');
+});
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in list" ref="items">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.test {
+  background: red;
+}
+</style>
