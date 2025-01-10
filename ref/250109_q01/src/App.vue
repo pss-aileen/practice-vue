98c0ce8c52906d29a438a439f6301fcf51ace7a7
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-10 10:55:52 +0900

    3.5以前のバージョン

---
+++ ref/250109_q01/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+
+const myInput: Ref<HTMLInputElement | null> = ref(null);
+
+function focusInput() {
+  if (myInput.value) {
+    myInput.value.focus();
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" ref="myInput" />
+    <button @click="focusInput">フォーカスを当てる</button>
+  </div>
+</template>
+
+<style scoped></style>
