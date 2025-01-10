e6df52958dd6a70985f6c3c51fae29cf0ee1a105
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-10 11:11:06 +0900

    q02 done

---
+++ useTemplateRef/250110_q02/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, useTemplateRef } from 'vue';
+
+const input1 = useTemplateRef('input1');
+const input2 = useTemplateRef('input2');
+
+function focusSecondInput() {
+  input2.value?.focus();
+}
+
+onMounted(() => {
+  input1.value?.focus();
+});
+</script>
+
+<template>
+  <div>
+    <input type="text" ref="input1" placeholder="1" />
+    <input type="text" ref="input2" placeholder="2" />
+    <button @click="focusSecondInput">2番目にフォーカス</button>
+  </div>
+</template>
+
+<style scoped></style>
