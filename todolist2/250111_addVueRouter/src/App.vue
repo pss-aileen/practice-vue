0df87cc1881dc4627db04fcacb07008672e06b0f
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-13 15:14:19 +0900

    separate home

--- todolist2/250111_addVueRouter/src/App.vue
+++ todolist2/250111_addVueRouter/src/App.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import InputForm from './components/InputForm.vue';
-import FilterSetting from './components/FilterSetting.vue';
-import TodoList from './components/TodoList.vue';
-import CategoryInput from './components/CategoryInput.vue';
 import { RouterLink } from 'vue-router';
 </script>
 
@@ -17,17 +13,8 @@ import { RouterLink } from 'vue-router';
       <RouterLink to="/single-todo">single-todo</RouterLink>
     </nav>
 
-    <RouterView />
-
     <div>
-      <main>
-        <InputForm />
-        <TodoList />
-      </main>
-      <aside>
-        <FilterSetting />
-        <CategoryInput />
-      </aside>
+      <RouterView />
     </div>
   </section>
 </template>
@@ -36,27 +23,5 @@ import { RouterLink } from 'vue-router';
 .wrapper {
   max-width: 800px;
   margin: 32px auto;
-
-  & > div {
-    display: flex;
-    gap: 32px;
-    margin-top: 32px;
-
-    main {
-      flex: 3;
-
-      & > * + * {
-        margin-top: 32px;
-      }
-    }
-
-    aside {
-      flex: 1;
-
-      & > * + * {
-        margin-top: 32px;
-      }
-    }
-  }
 }
 </style>
--- todolist2/250111_addVueRouter/src/HomeView.vue
+++ todolist2/250111_addVueRouter/src/HomeView.vue
@@ -1,7 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InputForm from './components/InputForm.vue';
+import FilterSetting from './components/FilterSetting.vue';
+import TodoList from './components/TodoList.vue';
+import CategoryInput from './components/CategoryInput.vue';
+</script>
 
 <template>
-  <div>home</div>
+  <div>
+    <main>
+      <InputForm />
+      <TodoList />
+    </main>
+    <aside>
+      <FilterSetting />
+      <CategoryInput />
+    </aside>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+div {
+  display: flex;
+  gap: 32px;
+  margin-top: 32px;
+
+  main {
+    flex: 3;
+
+    & > * + * {
+      margin-top: 32px;
+    }
+  }
+
+  aside {
+    flex: 1;
+
+    & > * + * {
+      margin-top: 32px;
+    }
+  }
+}
+</style>
