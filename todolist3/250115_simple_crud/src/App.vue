249bffcd4c9dd4fbeb2ca6819528f991ee2096ed
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-15 17:47:23 +0900

    set localstorage

--- todolist3/250115_simple_crud/src/App.vue
+++ todolist3/250115_simple_crud/src/App.vue
@@ -1,11 +1,147 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref, type Ref } from 'vue'
+
+/*
+  - 要素を作ってイメージを作る
+  - Vueで動きを実装する
+*/
+
+interface todosType {
+  id: number
+  title: string
+}
+
+const todos: Ref<todosType[]> = ref([])
+
+const todoTitleInput: Ref<string> = ref('')
+
+function addTodo() {
+  if (!todoTitleInput.value.trim()) {
+    return
+  }
+  todos.value.push({
+    id: new Date().getTime(),
+    title: todoTitleInput.value,
+  })
+
+  todoTitleInput.value = ''
+  updateLocalStorage()
+}
+
+function editTodo(id: number) {
+  todos.value = todos.value.map((todo) => {
+    if (todo.id === id) {
+      let newTodo = ''
+      do {
+        newTodo = prompt('Edit Title', todo.title) as string
+      } while (!newTodo)
+
+      todo.title = newTodo
+    }
+    return todo
+  })
+  updateLocalStorage()
+}
+
+function deleteTodo(id: number) {
+  todos.value = todos.value.filter((todo) => {
+    return todo.id !== id
+  })
+  updateLocalStorage()
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('myTodos', JSON.stringify(todos.value))
+}
+
+onMounted(() => {
+  const localStorageTodos = localStorage.getItem('myTodos')
+  if (localStorageTodos) {
+    todos.value = JSON.parse(localStorageTodos)
+  }
+})
+</script>
 
 <template>
-  <div></div>
+  <section class="wrapper">
+    <h1>Todo List</h1>
+
+    <section class="add-todo">
+      <h2>Add Todo</h2>
+      <form @submit.prevent="addTodo">
+        <input type="text" v-model="todoTitleInput" />
+        <button>Add</button>
+      </form>
+    </section>
+    <section class="todo-list">
+      <h2>List</h2>
+      <ul>
+        <li v-for="todo in todos" :key="todo.id">
+          <div>{{ todo.title }}</div>
+          <div>
+            <button @click="editTodo(todo.id)">edit</button>
+            <button @click="deleteTodo(todo.id)">delete</button>
+          </div>
+        </li>
+      </ul>
+    </section>
+  </section>
 </template>
 
 <style lang="scss" scoped>
-body {
-  text-align: center;
+.wrapper {
+  max-width: 400px;
+  margin: 48px auto;
+
+  h1 {
+    font-size: 32px;
+    text-align: center;
+  }
+
+  & > * + section {
+    margin-top: 32px;
+  }
+
+  & > section {
+    h2 {
+      font-size: 16px;
+    }
+    h2 + * {
+      margin-top: 16px;
+    }
+  }
+
+  .add-todo {
+    form {
+      display: flex;
+      input {
+        flex: 1;
+      }
+      button {
+      }
+    }
+  }
+
+  .todo-list {
+    ul {
+      list-style: none;
+      padding: 0;
+
+      li {
+        display: flex;
+        gap: 8px;
+
+        div:nth-of-type(1) {
+          flex: 1;
+        }
+        div:nth-of-type(1) {
+        }
+      }
+
+      li + li {
+        margin-top: 8px;
+      }
+    }
+  }
 }
 </style>
